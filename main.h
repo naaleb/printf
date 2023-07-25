654e9eb99c0d54304c1b3cbd0933028845fe3370
Hermas Naalebkuur <naalebkuurh@gmail.com>
2023-07-25 05:21:30 -0700

    main.h updated

--- main.h
+++ main.h
@@ -8,6 +8,21 @@
   *By Hermas and Almond
   */
 
+#define UNUSED(x)(void)(x)
+#define BUFF_SIZE 1024
+
+/* SIZES */
+#define S_LONG 2
+#define S_SHORT 1
+
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+
 int _printf(const char *format, ...);
 
 #endif  
