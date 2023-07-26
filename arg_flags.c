d2424c0a5357ca0936a4a61eb7f600dfdcbac32d
Hermas Naalebkuur <53911569+naaleb@users.noreply.github.com>
2023-07-26 20:54:17 +0000

    Update and rename get_flags.c to arg_flags.c

--- arg_flags.c
+++ arg_flags.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * get_flags - Calculates active flags
- * @format: Formatted string in which to print the arguments
+ * get_flags - function to calculates active flags
+ * @format: Formatted string to print arguments
  * @i: take a parameter.
- * Return: Flags:
+ * Return: Return flags
+ *
+ * By Hermas and Almond
  */
 int get_flags(const char *format, int *i)
 {
-	/* - + 0 # ' ' */
-	/* 1 2 4 8  16 */
 	int j, curr_i;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
