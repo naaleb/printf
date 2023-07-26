2c0f4f7e46500a5738de967d68cbc7576efdfcb1
Hermas Naalebkuur <53911569+naaleb@users.noreply.github.com>
2023-07-26 21:10:15 +0000

    Updated and rename get_size.c to size_handler.c

--- size_handler.c
+++ size_handler.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * get_size - Calculates the size to cast the argument
+ * get_size -Function to calculate the size to cast the argument
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  *
  * Return: Precision.
+ * By Hermas and Almond
  */
 int get_size(const char *format, int *i)
 {
