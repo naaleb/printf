bdcaa2132dd3c53d079dbe25354f69414e1bfe5b
Hermas Naalebkuur <53911569+naaleb@users.noreply.github.com>
2023-07-26 20:36:35 +0000

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -3,9 +3,11 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function
+ * _printf - function that produces output according to a format
  * @format: format.
  * Return: Printed chars.
+ *
+ * By Hermas and Almond
  */
 int _printf(const char *format, ...)
 {
@@ -26,7 +28,6 @@ int _printf(const char *format, ...)
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
