6ca955cd7cd7a81a896bc8de4953329c5428f6e2
Hermas Naalebkuur <naalebkuurh@gmail.com>
2023-07-27 06:16:46 -0700

    _printf updated

--- _printf.c
+++ _printf.c
@@ -5,13 +5,12 @@ void print_buffer(char buffer[], int *buff_ind);
  * _printf - function that produces output according to a format
  * @format: format.
  * Return: Printed chars.
- *
  * By Hermas and Almond
  */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
+	int i, printed = 0, printed_chars = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
 
